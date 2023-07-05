9489439321d0d6c8f85cc801d538f5d5acdb9f8f
Yogesh <yogesh.mopada@ishafoundation.org>
2023-07-06 01:13:05 +0530

    updated the sitenamme in loginpage

--- src/components/Pages/LoginPage/LoginPage.jsx
+++ src/components/Pages/LoginPage/LoginPage.jsx
@@ -5,7 +5,7 @@ export default function LoginPage(){
     return (
         <>
         <div style={{marginTop:"250px",alignItems:"center",textAlign:"center"}}>
-        <h1>Earth Engine Web App</h1>
+        <h1>EARTHLINGS</h1>
         <button style={{ width:"100px", margin:"auto"}}onClick={()=> loginWithRedirect()}>Login</button>
         </div>
         </>
